e0a8f1531935a25bf4f2dc7e3a38bdee4e4bdcfc
Anni <nadeka@users.noreply.github.com>
2015-09-16 06:33:34 +0300

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-cd ~/pizzataivas/
-
-rsync -z -r app assets config lib sql vendor index.php composer.json ~/public_html/pizzataivas/
-
-cd ~/public_html/pizzataivas/
-php composer.phar dump-autoload
-exit
